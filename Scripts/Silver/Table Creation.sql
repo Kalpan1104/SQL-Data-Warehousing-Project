2d6dc5b0726c261cf2dc786af86879d0f3ff52df
Kalpan1104 <61786599+Kalpan1104@users.noreply.github.com>
2025-12-30 14:51:05 -0500

    Alter prd_start_dt and prd_end_dt to use DATE type
    Changed data types for prd_start_dt and prd_end_dt from DATETIME to DATE.

--- Scripts/Silver/Table Creation.sql
+++ Scripts/Silver/Table Creation.sql
@@ -28,8 +28,8 @@ create table if not exists silver_crm_prd_info (
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt   DATE,
     dwh_create_date     DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
