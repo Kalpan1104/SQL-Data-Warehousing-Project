9ba9b37f9c7599a28cdf3d3500b862e61b0e3abe
Kalpan1104 <61786599+Kalpan1104@users.noreply.github.com>
2025-12-30 14:54:42 -0500

    Modify silver_crm_prd_info table structure

--- Scripts/Silver/Table Creation.sql
+++ Scripts/Silver/Table Creation.sql
@@ -22,15 +22,16 @@ create table if not exists silver_crm_cust_info (
     dwh_create_date     DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-create table if not exists silver_crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATE,
-    prd_end_dt   DATE,
-    dwh_create_date     DATETIME DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS silver_crm_prd_info (
+    prd_id          INT,
+    cat_id         	VARCHAR(50),
+    prd_key        	VARCHAR(50),
+    prd_nm			VARCHAR(50),
+    prd_cost        INT,
+    prd_line        VARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 create table if not exists silver_crm_sales_details (
